6d982ccc78e4fc7fa9c7350a8c1166efdf9422d4
njwongzhe <ngjaawei@gmail.com>
2026-06-22 20:42:29 +0800

    Authentication Module Updated

--- gamma-3/public/js/pages/auth-login-page.js
+++ gamma-3/public/js/pages/auth-login-page.js
@@ -64,7 +64,7 @@ export default {
                     <div class="input-field-container">
                         <div class="input-field">
                             <label class="input-box-label" for="phoneNumber">Phone Number</label>
-                            <input class="input-box" id="phoneNumber" v-model="phoneNumber" type="text" placeholder="012-3456789" />
+                            <input class="input-box" id="phoneNumber" v-model="phoneNumber" type="text" placeholder="0123456789" />
                         </div>
                         <div class="input-field">
                             <label class="input-box-label" for="password">Password</label>
